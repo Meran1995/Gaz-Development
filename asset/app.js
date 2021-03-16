b6202b0a5383fde046a555de9032b58ee9e25f22
Meran <meranzakelijk@gmail.com>
2021-03-16 02:59:43 +0100

    did a animation (type wright the text) with JS

--- asset/app.js
+++ asset/app.js
@@ -1,14 +1,67 @@
-$(document).ready(function(){
-	$(window).scroll(function(){
-		if(this.scrollY > 20){
-			$(".navbar").addClass("sticky");
-		}else{
-			$(".navbar").removeClass("sticky");
-			
+const aWork = () => {
+	const TypeWriter = function (textElement, words, wait = 1000) {
+		this.textElement = textElement;
+		this.words = words;
+		this.txt = "";
+		this.wordIndex = 0;
+		this.wait = parseInt(wait, 10);
+		this.type();
+		this.isDeleting = false;
+	};
+
+	// type Method
+	TypeWriter.prototype.type = function () {
+		//current index
+		const current = this.wordIndex % this.words.length;
+		// full text
+		const fullTxt = this.words[current];
+		// if deleted
+		if (this.isDeleting) {
+			//remove
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			//add
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
 		}
-	});
-	// toggle menu/navbarscript
-	$(".hamburger-btn").click(function(){
-		$(".header .navbar .max-width .margin-around .menu").toggleClass("active");
-	});
-})
+
+		// insert txt into Element
+		this.textElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+		// Type Speed
+		let typeSpeed = 80;
+
+		if (this.isDeleting) {
+			typeSpeed /= 2;
+		}
+
+		// complite txt
+		if (!this.isDeleting && this.txt === fullTxt) {
+			//make pause
+			typeSpeed = this.wait;
+			//set to true
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === "") {
+			this.isDeleting = false;
+			// next word
+			this.wordIndex++;
+			//pause
+			typeSpeed = 300;
+		}
+
+		setTimeout(() => this.type(), typeSpeed);
+	};
+	// Init on DOM LOAD
+	document.addEventListener("DOMContentLoaded", init);
+
+	// init App
+	function init() {
+		const textElement = document.querySelector(".a-work");
+		const words = JSON.parse(textElement.getAttribute("data-words"));
+		const wait = textElement.getAttribute("data-wait");
+		// init typeWriter
+		new TypeWriter(textElement, words, wait);
+	}
+};
+
+
+aWork();
